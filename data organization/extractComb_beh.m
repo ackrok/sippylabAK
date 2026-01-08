cc2f5e71edd1c25c29f1435a314d2fc937a29758
Krok <ack466@nyumc.org>
2026-01-08 15:51:50 -0500

    updates

--- data organization/extractComb_beh.m
+++ data organization/extractComb_beh.m
@@ -25,9 +25,9 @@ function comb = extractComb_beh(varargin)
 %%
 switch nargin
     case 1
-        combBeh = varargin{1}; % If comb is provided, use it to store data
+        comb = varargin{1}; % If comb is provided, use it to store data
     case 0
-        combBeh = struct; % Initiate structure to store data such that works with other plotting scripts
+        comb = struct; % Initiate structure to store data such that works with other plotting scripts
 end
 
 %%
@@ -60,10 +60,10 @@ for a = 1:length(filePath)
     mouse = regexp(str, 'JT0\d{2}', 'match', 'once'); % extract JT followed by 0 and two digits (e.g. JT019)
     date = regexp(str, '\d{6}', 'match', 'once'); % extract any sequence of exactly six digits (e.g. 251215)
 
-    b = size(combBeh,2); if isempty(fieldnames(combBeh)); b = 0; end
-    combBeh(b+1).mouse = mouse; 
-    combBeh(b+1).date = date;
-    combBeh(b+1).beh = beh; % Store behavioral data in the structure
+    b = size(comb,2); if isempty(fieldnames(comb)); b = 0; end
+    comb(b+1).mouse = mouse; 
+    comb(b+1).date = date;
+    comb(b+1).beh = beh; % Store behavioral data in the structure
     toc
     fprintf('Extracted behavioral data for: %s-%s \n',mouse,date);
 end
